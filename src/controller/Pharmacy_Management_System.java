1fae9f9d8baacd28105e369958facb41e9cd0159
Asus ROG <Asus ROG@LAPTOP-DVTMLRSO>
2020-06-14 12:21:26 +0530

    this is first commite

---
+++ src/controller/Pharmacy_Management_System.java
@@ -0,0 +1,20 @@
+package controller;
+
+import view.Login;
+
+
+
+
+
+public class Pharmacy_Management_System {
+
+ 
+    public static void main(String[] args) {
+        
+        // This is Main Class
+        Login l1 = new Login();
+        l1.show(true);
+        
+    }
+    
+}
